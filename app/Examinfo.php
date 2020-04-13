e6188cfd02d92a2128546f3eb5f4597be26c296c
Amyth Kumar <kumar.amyth@gmail.com>
2020-04-13 16:55:34 +0530

    adding comment

--- app/Examinfo.php
+++ app/Examinfo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class examinfo extends Model
 {
-    //
+    //table details to fill 
     protected $fillable = [
         'Teacher_id', 'Course', 'question_lenth','qualifying_marks','uniqueid','time',
     ];
